62a589933387c003f14654c1ebdafe0d3556558a
victor bruschelli <165551238+VictorBruschelli@users.noreply.github.com>
2024-06-03 20:12:24 +0200

    added printf

---
+++ src/main/java/org/lessons/java/HelloJava.java
@@ -0,0 +1,11 @@
+package org.lessons.java;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("hello java");
+
+	}
+
+}
